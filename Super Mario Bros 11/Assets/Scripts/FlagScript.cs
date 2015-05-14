18dccd9ebe12ce8684f53cd7e98eeecfe382c780
Racksay <edvin.klaebo@gmail.com>
2015-05-14 16:25:35 +0200

    Revert d26cb03..27487c6
    This rolls back to commit d26cb035ccd7c60c8c2aae223905822b521b1b8d.

--- Super Mario Bros 11/Assets/Scripts/FlagScript.cs
+++ Super Mario Bros 11/Assets/Scripts/FlagScript.cs
@@ -6,57 +6,20 @@ public class FlagScript : MonoBehaviour {
     Transform player;
     Kontroller2D kontroller;
     Spiller spiller;
-    Rigidbody2D rigidbody;
-    Animator anim;
 
-
-    bool stopPlayer = true;
-    bool moveDown = false;
-    bool moveLeft = false;
-    bool end = false;
 
 
 
 	// Use this for initialization
 	void Start () {
-	    
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (player != null && stopPlayer)
-        {
-            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
-            stopPlayer = false;
-            moveDown = true;
-        }
-        if (moveDown)
-        {
-            print("hello");
-            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 2f * Time.deltaTime, player.transform.position.z);
-            if (player.transform.position.y < 1f)
-            {
-                moveDown = false;
-                moveLeft = true;
-            }
-        }
-        if (moveLeft && anim != null)
-        {
-           // print("hello");
-
-            player.transform.position = new Vector3(player.transform.position.x + 2f * Time.deltaTime, player.transform.position.y, player.transform.position.z);
-            anim.SetBool("Grounded", true);
-            anim.SetFloat("Speed", 1f);
-            if (player.transform.position.x > 204f)
-            {
-                moveLeft = false;
-                anim.SetFloat("Speed", 0f);
-                Invoke("LoadNextLevel", 5);
-            }
-        }
-
-
+        if(player != null)
+        player.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 
 
 
@@ -69,17 +32,8 @@ public class FlagScript : MonoBehaviour {
         player = other.GetComponent<Transform>();
         kontroller = other.GetComponent<Kontroller2D>();
         spiller = other.GetComponent<Spiller>();
-        rigidbody = other.GetComponent<Rigidbody2D>();
-        anim = other.GetComponent<Animator>();
-         
-        Destroy(rigidbody);
         Destroy(spiller);
         Destroy(kontroller);
 
-    }
-
-    void LoadNextLevel()
-    {
-        Application.LoadLevel(0);
     }
 }
