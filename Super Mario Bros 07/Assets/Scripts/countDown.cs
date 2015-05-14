a6f2a238b4558316301bf9ed5c942616e437a78c
johrobert <johnursin@hotmail.com>
2015-05-14 14:52:27 +0200

    GUI
    La til ny font, forandret countDown script, og la til prefab for GUI

--- Super Mario Bros 07/Assets/Scripts/countDown.cs
+++ Super Mario Bros 07/Assets/Scripts/countDown.cs
@@ -1,40 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class countDown : MonoBehaviour {
-	GUIStyle fontSize;
-	public int Size;
-	public Font font;
-	public float timeFloat = 5;
 
-	public int left;
-	public int top;
-
-    GUIText text;
-
+	private float timeFloat = 399;
+	public Text timer;
+	int score = 000000;
 	int time = 0;
+
+
+
 	// Use this for initialization
 	void Start () {
-		fontSize = new GUIStyle();
-		fontSize.fontSize = Size;
-        text = GetComponent<GUIText>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        text.text = "TIME: " + time;
-
+		timer.text = "" + time;
 		if (timeFloat > 1) {
 			timeFloat -= Time.deltaTime;
 		} else {
 			timeFloat = 0;
 		}
 		time = Mathf.FloorToInt(timeFloat);
+
+
 	}
-	void OnGUI(){
-		GUI.skin.font = font;
-		fontSize.normal.textColor = Color.white;
-			GUI.Label (new Rect (left, top, 200, 100), ""+time , fontSize);
-	}	
+
 }
